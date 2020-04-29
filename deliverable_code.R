a90960dbf0db7da29c449fa51d137c0660a8a908
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-04-29 12:02:19 -0500

    petitioned script with headers from Weber

---
+++ deliverable_code.R
@@ -0,0 +1,13 @@
+# Preamble
+library(tidyverse)
+library(GGally)
+
+# Loading and Processing the Data
+train <- read_csv("Data/final_train.csv", col_types = "dfffdddddddddddddf")
+compete <- read_csv("Data/final_compete.csv", col_types = "dfffddddddddddddd")
+
+# Building the Models
+
+# Testing the Model and Results
+
+# Deployment
